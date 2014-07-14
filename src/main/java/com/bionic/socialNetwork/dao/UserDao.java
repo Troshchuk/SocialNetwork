a9d417fca72a793366e869620515d81a8094f539
Dmytro Troshchuk <shtorm308@gmail.com>
2014-07-14 11:41:16 +0300

    Create structure of project

---
+++ src/main/java/com/bionic/socialNetwork/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.bionic.socialNetwork.dao;
+
+/**
+ * @author Dmytro Troshchuk
+ * @version 1.00  14.07.14.
+ */
+public interface UserDao {
+}
---
+++ src/main/java/com/bionic/socialNetwork/models/User.java
@@ -0,0 +1,8 @@
+package com.bionic.socialNetwork.models;
+
+/**
+ * @author Dmytro Troshchuk
+ * @version 1.00  14.07.14.
+ */
+public class User {
+}
