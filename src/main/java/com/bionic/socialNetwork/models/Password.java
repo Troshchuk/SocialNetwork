1b9ea77e4fe43c357abfa5b47b534e60e2747f10
Dmytro Troshchuk <shtorm308@gmail.com>
2014-07-15 00:50:46 +0300

    Add entity: Password

---
+++ src/main/java/com/bionic/socialNetwork/models/Password.java
@@ -0,0 +1,49 @@
+package com.bionic.socialNetwork.models;
+
+import javax.persistence.*;
+
+/**
+ * Password entity
+ *
+ * @author Dmytro Troshchuk
+ * @version 1.00  14.07.14.
+ */
+@Entity
+@Table(name = "Passwords")
+public class Password {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "user_id")
+    private long userId;
+
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Password() {
+
+    }
+
+    public Password(long userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
