7ded3336a2b4fa66b2c3b70e72b2dd3b838b01a2
Dmytro Troshchuk <shtorm308@gmail.com>
2014-07-15 15:45:44 +0300

    SN-37 Add Junit test for method getUserById

--- src/test/java/com/bionic/socialNetwork/dao/impl/UserDaoImplTest.java
+++ src/test/java/com/bionic/socialNetwork/dao/impl/UserDaoImplTest.java
@@ -4,6 +4,8 @@ import com.bionic.socialNetwork.dao.UserDao;
 import com.bionic.socialNetwork.models.User;
 import org.junit.Test;
 
+import javax.jws.soap.SOAPBinding;
+
 import static org.junit.Assert.*;
 
 public class UserDaoImplTest {
@@ -14,5 +16,13 @@ public class UserDaoImplTest {
 
         UserDao userDao = new UserDaoImpl();
         userDao.addUser(user);
+    }
+
+    @Test
+    public void testGetUserById() throws Exception {
+        long id = 1;
+        UserDao userDao = new UserDaoImpl();
+        User user = userDao.getUserById(id);
+        assertNotNull(user);
     }
 }
