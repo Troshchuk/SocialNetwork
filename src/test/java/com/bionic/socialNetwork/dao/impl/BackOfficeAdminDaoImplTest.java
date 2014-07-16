7284dd76685f56e592f1591e7d3093842ce38535
Dmytro Troshchuk <shtorm308@gmail.com>
2014-07-16 17:31:51 +0300

    SN-39 Implement JUnit test for Back Office Administrator DAO

---
+++ src/test/java/com/bionic/socialNetwork/dao/impl/BackOfficeAdminDaoImplTest.java
@@ -0,0 +1,45 @@
+package com.bionic.socialNetwork.dao.impl;
+
+import com.bionic.socialNetwork.dao.BackOfficeAdminDao;
+import com.bionic.socialNetwork.dao.UserDao;
+import com.bionic.socialNetwork.models.BackOfficeAdmin;
+import com.bionic.socialNetwork.models.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BackOfficeAdminDaoImplTest {
+
+    private User user;
+    private BackOfficeAdminDao backOfficeAdminDao;
+
+    @Before
+    public void testInsert() throws Exception {
+        UserDao userDao = new UserDaoImpl();
+        user = new User("BackAdminTest", "", "", "");
+        userDao.insert(user);
+        backOfficeAdminDao = new BackOfficeAdminDaoImpl();
+
+        BackOfficeAdmin backOfficeAdmin = new BackOfficeAdmin(user.getId());
+        backOfficeAdminDao.insert(backOfficeAdmin);
+
+
+    }
+
+    @Test
+    public void testSelectAll() throws Exception {
+        List<BackOfficeAdmin> backOfficeAdmins = backOfficeAdminDao.selectAll();
+        BackOfficeAdmin backOfficeAdmin = backOfficeAdminDao.selectAll().get(0);
+        assertNotNull(backOfficeAdmin);
+    }
+
+    @After
+    public void testDelete() throws Exception {
+        BackOfficeAdmin backOfficeAdmin = new BackOfficeAdmin(user.getId());
+        backOfficeAdminDao.delete(backOfficeAdmin);
+    }
+}
