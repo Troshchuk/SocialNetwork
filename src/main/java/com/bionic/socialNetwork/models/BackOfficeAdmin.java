cc14f36adb7875eb7641d5314411ebbd3cf0edad
Dmytro Troshchuk <shtorm308@gmail.com>
2014-07-16 17:28:52 +0300

    SN-31 Create Back Office administrator entity

---
+++ src/main/java/com/bionic/socialNetwork/models/BackOfficeAdmin.java
@@ -0,0 +1,45 @@
+package com.bionic.socialNetwork.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+/**
+ * @author Dmytro Troshchuk
+ * @version 1.00  16.07.14.
+ */
+@Entity(name = "Back_Office_Administrators")
+public class BackOfficeAdmin {
+    @Id
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    public BackOfficeAdmin() {
+
+    }
+
+    public BackOfficeAdmin(long id) {
+        this.id = id;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
