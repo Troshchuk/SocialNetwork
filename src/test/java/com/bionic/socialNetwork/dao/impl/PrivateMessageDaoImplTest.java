4fcab538d50ccbcb12f8a175196c5b1f7668d485
DenisBiyovskiy <mrkronosalive@gmail.con>
2014-07-17 10:55:06 +0300

    Refactoring

---
+++ src/test/java/com/bionic/socialNetwork/dao/impl/PrivateMessageDaoImplTest.java
@@ -0,0 +1,51 @@
+package com.bionic.socialNetwork.dao.impl;
+
+import com.bionic.socialNetwork.dao.PrivateMessageDao;
+import com.bionic.socialNetwork.dao.UserDao;
+import com.bionic.socialNetwork.models.Password;
+import com.bionic.socialNetwork.models.PrivateMessage;
+import com.bionic.socialNetwork.models.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Denis
+ * @version 1.00  16.07.2014.
+ */
+public class PrivateMessageDaoImplTest {
+    private User sentUser;
+    private User receivedUser;
+    private PrivateMessageDao privateMessageDao;
+    private long messageId;
+
+    @Before
+    public void testInsert() throws Exception {
+        UserDao userDao = new UserDaoImpl();
+        sentUser = new User("PostUser", "", "", "");
+        userDao.insert(sentUser, new Password("root"));
+        receivedUser = new User("PostUser", "", "", "");
+        userDao.insert(receivedUser, new Password("root"));
+
+        privateMessageDao = new PrivateMessageDaoImpl();
+        PrivateMessage privateMessage = new PrivateMessage(sentUser, receivedUser, "Some message");
+        privateMessageDao.insert(privateMessage);
+
+        messageId = privateMessage.getMessageId();
+        assertEquals(privateMessage.getMessageId(), privateMessageDao.selectBySentId(messageId).getMessageId());
+    }
+
+    @Test
+    public void testSelectByReceiverId() throws Exception {
+        PrivateMessage privateMessage = privateMessageDao.selectByReceiverId(messageId);
+        assertNotNull(privateMessage);
+    }
+
+    @Test
+    public void testSelectBySentId() throws Exception {
+        PrivateMessage privateMessage = privateMessageDao.selectBySentId(messageId);
+        assertNotNull(privateMessage);
+    }
+}
