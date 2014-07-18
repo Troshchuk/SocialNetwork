00d0f23fa04f325aebfb47c433507900b68dd8a3
yoalex5 <yoalex5@gmail.com>
2014-07-18 10:36:54 +0300

    SN-22 1.mapped groups entities 2. added equals() and hashCode() methods to Groups objects

--- src/main/java/com/bionic/socialNetwork/models/GroupPost.java
+++ src/main/java/com/bionic/socialNetwork/models/GroupPost.java
@@ -66,4 +66,28 @@ public class GroupPost {
     public void setPost(String post) {
         this.post = post;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupPost groupPost = (GroupPost) o;
+
+        if (groupId != groupPost.groupId) return false;
+        if (groupPostId != groupPost.groupPostId) return false;
+        if (userId != groupPost.userId) return false;
+        if (post != null ? !post.equals(groupPost.post) : groupPost.post != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (groupPostId ^ (groupPostId >>> 32));
+        result = 31 * result + (int) (groupId ^ (groupId >>> 32));
+        result = 31 * result + (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (post != null ? post.hashCode() : 0);
+        return result;
+    }
 }
