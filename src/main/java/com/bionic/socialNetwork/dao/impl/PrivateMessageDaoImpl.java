caa44ad97b75fb9b86a71437f383020f1b29f954
DenisBiyovskiy <mrkronosalive@gmail.con>
2014-07-21 14:38:46 +0300

    Refactor

--- src/main/java/com/bionic/socialNetwork/dao/impl/PrivateMessageDaoImpl.java
+++ src/main/java/com/bionic/socialNetwork/dao/impl/PrivateMessageDaoImpl.java
@@ -19,60 +19,77 @@ public class PrivateMessageDaoImpl implements PrivateMessageDao {
 
     @Override
     public PrivateMessage selectBySentId(long id) throws Exception {
-
         Session session = HibernateUtil.getSessionFactory().openSession();
-        PrivateMessage privateMessage = (PrivateMessage) session.get(PrivateMessage.class, id);
-        session.close();
-        return privateMessage;
+        try {
+            PrivateMessage privateMessage = (PrivateMessage) session.get(PrivateMessage.class, id);
+            return privateMessage;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public PrivateMessage selectByReceiverId(long id) throws Exception {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        PrivateMessage privateMessage = (PrivateMessage) session.get(PrivateMessage.class, id);
-        session.close();
-        return privateMessage;
+        try {
+            PrivateMessage privateMessage = (PrivateMessage) session.get(PrivateMessage.class, id);
+            return privateMessage;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<PrivateMessage> selectNextSentId(long beginId) throws Exception {
         int limit = 10;
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery(
-                "FROM PrivateMessage WHERE sentUser >= " + beginId + " AND sentUser < " +
-                        (beginId + limit));
-        List<PrivateMessage> privateMessages = query.list();
-        session.close();
-        return privateMessages;
+        try {
+            Query query = session.createQuery(
+                    "FROM PrivateMessage WHERE sentUser >= " + beginId + " AND sentUser < " +
+                            (beginId + limit));
+            List<PrivateMessage> privateMessages = query.list();
+            return privateMessages;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<PrivateMessage> selectNextReceiverId(long beginId) throws Exception {
         int limit = 10;
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery(
-                "FROM PrivateMessage WHERE receiverUser >= " + beginId + " AND receiverUser < " +
-                        (beginId + limit));
-        List<PrivateMessage> privateMessages = query.list();
-        session.close();
-        return privateMessages;
+        try {
+            Query query = session.createQuery(
+                    "FROM PrivateMessage WHERE receiverUser >= " + beginId + " AND receiverUser < " +
+                            (beginId + limit));
+            List<PrivateMessage> privateMessages = query.list();
+            return privateMessages;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void insert(PrivateMessage privateMessage) throws Exception {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.save(privateMessage);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(privateMessage);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void delete(PrivateMessage privateMessage) throws Exception {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        session.delete(privateMessage);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.delete(privateMessage);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 }
