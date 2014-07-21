717b527548fc75c6d1e9d78c5f3c160ed7b40119
Dmytro Troshchuk <shtorm308@gmail.com>
2014-07-21 21:54:30 +0300

    SN-64 Implement registration page

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -26,6 +26,29 @@ jQuery(function( $ ) {
 		}
 	});
 
+//    @FormParam("name") String name,
+//        @FormParam("surname") String surname,
+//        @FormParam("email") String login,
+//        @FormParam("password") String password,
+//        @FormParam("invite") String invite) {
+//
+    $('#register-btn2').click(function() {
+        name = $('#name').val();
+        surname = $('#surname').val();
+        email = $('#email').val();
+        pass = $('#pass').val();
+        invite = $('#invite').val();
+//        if(login!="" && pass!="") {
+////			$.post('ajax/index.php',{login:login,pass:pass},function(server_json){
+            $.post('/sn/user/registration',{name:name,surname:surname,email:email,password:pass,invite:invite},function(server_json){
+                if(server_json.status==true) {
+                    location.reload();
+                }
+                else {alert('Wrong login or password')}
+            },'json');
+//        }
+    });
+
 	var $wrapper = $(".main-section"),
 		$header = $(".main-header"),
 		$footer = $(".main-footer");
