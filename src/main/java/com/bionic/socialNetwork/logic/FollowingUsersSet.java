269733832dc445343432700700fb0d875d7043d9
DenisBiyovskiy <mrkronosalive@gmail.com>
2014-07-23 11:04:57 +0300

    SN-79:  SN-82 Create logic for following.

---
+++ src/main/java/com/bionic/socialNetwork/logic/FollowingUsersSet.java
@@ -0,0 +1,44 @@
+package com.bionic.socialNetwork.logic;
+
+import com.bionic.socialNetwork.dao.UserDao;
+import com.bionic.socialNetwork.models.User;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author Denis Biyovskiy
+ * @version 1.00  23.07.2014.
+ */
+public class FollowingUsersSet {
+    @JsonIgnore
+    private User user;
+    @JsonIgnore
+    private UserDao userDao;
+    @JsonIgnore
+    private int userId;
+    @JsonIgnore
+    private int beginId;
+
+    private Set<User> followingUsers;
+
+    public FollowingUsersSet(int beginId, int userId) {
+        this.beginId = beginId;
+        this.userId = userId;
+    }
+
+    public void pullFollowing(int beginId) throws Exception {
+        user = userDao.selectById(userId);
+        Set<User> followingUsersAll = user.getFriends();
+        Iterator itr = followingUsersAll.iterator();
+        for (int i = 0; i < beginId ; i++, itr.next());
+        for (int i = beginId; i < beginId + 10; i++) {
+            followingUsers.add((User)itr.next());
+        }
+    }
+
+    public Set<User> getFollowingUsers() {
+        return followingUsers;
+    }
+}
