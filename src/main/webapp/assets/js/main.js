853790838a4191a12fc244c4434604f30bc9dc2b
oleksandrD <allis4you@gmail.com>
2014-07-30 15:05:14 +0400

    Implement News Page(no styles); Implement Group Page; Implement Messages page

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -24,12 +24,6 @@ jQuery(function( $ ) {
 		}
 	});
 
-//    @FormParam("name") String name,
-//        @FormParam("surname") String surname,
-//        @FormParam("email") String login,
-//        @FormParam("password") String password,
-//        @FormParam("invite") String invite) {
-//
     $('#register-btn2').click(function() {
         var name = $('#name').val();
         var surname = $('#surname').val();
@@ -54,6 +48,16 @@ jQuery(function( $ ) {
         },'json');
     });
 
+	$('.messages-tab').click(function(event){
+		event.preventDefault();
+		var tab = $(this);
+		$('.messages-tab').removeClass('active')
+		tab.addClass('active')
+		
+		var id = tab.attr('href');
+		$('.messages-tab-content').hide();
+		$(id).fadeIn();
+	});
 
 	$('#exit').click('click', function() {
 		$.get('/sn/user' +getUserId()+ '/exit', {}, function(response){
@@ -99,7 +103,8 @@ jQuery(function( $ ) {
 
 	function formatDate(msec) {
 		var date = new Date(msec);
-		var formated = 'at ' + date.getHours() + ':' + date.getMinutes() + ' on ' + date.getDate() + '.' + date.getMonth()+1 + '.' + date.getFullYear();
+		var month = ( '0' + ( date.getMonth()+1 ) ).slice(-2);
+		var formated = 'at ' + date.getHours() + ':' + date.getMinutes() + ' on ' + date.getDate() + '.' + month + '.' + date.getFullYear();
 		return formated;
 	};
 
@@ -115,7 +120,6 @@ jQuery(function( $ ) {
 	}
 
 	function removePost(id) {
-		// console.log(id);
 		$.post('/sn/user' +getUserId()+ '/deletePost', {postId: id}, function(response){
 			if(response.status == true ) {
 				location.reload();
@@ -123,8 +127,42 @@ jQuery(function( $ ) {
 		});
 	};
 
+var page = 1;
 
+	function loadMorePosts() {
+		var fullname = '';
+		var position = '';
+		$('#wall').append( '<div class="wait"><img src="/assets/img/wait.gif" alt=""></div>' );
+		setTimeout( function(){ 
+			$.getJSON('/sn/user' + getUserId() + '/posts'+page , {}, function(json) {
+				for (var i = 0; i < json.posts.length; i++) {
+					var node = '<div class="post" id="' + json.posts[i].postId + '">';
+						node += '<div class="post-photo">';
+						node +=	'<a href="#">';
+						node += '<img src="/assets/img/Mt-8_dgwlHM.jpg" alt="">';
+						node += '</a>';
+						node += '</div>';
+						node += '<div class="post-message">';
+						node += '<p class="post-name"><a href="">' + fullname + '</a></p>';
+						node += '<span class="remove-post"></span>';
+						node +=	'<p>' + json.posts[i].post + '</p>';
+						node += '<span class="post-meta">'+formatDate( json.posts[i].time )+'</span>';
+						node += '</div>';
+						node += '</div>';
+						$('.wait').remove();
+						$('#wall').append( node );
+				};
+				$('.remove-post').on('click', function(){
+					var id = $(this).parents('.post').attr('id');
+					removePost(id);
+				});
+			});
+		}, 300 );
 
+		page++;
+	};
+
+	setInterval( function(){ loadMorePosts(); } , 5000);
 
 
 	function setupHomePage() {
@@ -148,7 +186,7 @@ jQuery(function( $ ) {
 		});
 
 
-		$.getJSON('/sn/user' + getUserId()+ '/posts0' , {}, function(json) {
+		$.getJSON('/sn/user' + getUserId() + '/posts0' , {}, function(json) {
 			for (var i = 0; i < json.posts.length; i++) {
 				var node = '<div class="post" id="' + json.posts[i].postId + '">';
 					node += '<div class="post-photo">';
@@ -177,12 +215,12 @@ jQuery(function( $ ) {
 		$.getJSON('ajax/messages.json', {}, function(json) {
 			for (var i = 0; i < json.messages.length; i++) {
 				var node = '<div class="message">';
-				node += '<div class="user-image"><a href=""><img src="" alt=""></a></div>'
-				node += '<div class="message-text">';
-				node += '<p>' + json + '</p>';
-				node += '<span class="meta">' + formatDate( getjson.date )+ '</span>';
-				node += '</div>';
-				node += '</div>';
+					node += '<div class="user-image"><a href=""><img src="" alt=""></a></div>'
+					node += '<div class="message-text">';
+					node += '<p>' + json + '</p>';
+					node += '<span class="meta">' + formatDate( getjson.date )+ '</span>';
+					node += '</div>';
+					node += '</div>';
 				$('#incoming').append( $(node) );
 			};
 		});
@@ -204,6 +242,42 @@ jQuery(function( $ ) {
 		});
 	};
 
+	function setupNewsPage() {
+		console.log('Setup News');
+		$.getJSON('/sn/news/news0', {}, function (json) {
+			for (var i = 0; i < json.posts.length; i++) {
+				var node = '<div class="news" id="' + json.posts[i].postId + '">';
+					// node += '<div class="post-photo">';
+					// node +=	'<a href="#">';
+					// node += '<img src="/assets/img/Mt-8_dgwlHM.jpg" alt="">';
+					// node += '</a>';
+					// node += '</div>';
+					node += '<div class="news-message">';
+					// node += '<p class="news-name"><a href="">' + fullname + '</a></p>';
+					// node += '<span class="remove-post"></span>';
+					node +=	'<p>' + json.posts[i].post + '</p>';
+					node += '<span class="post-meta">'+formatDate( json.posts[i].time )+'</span>';
+					node += '</div>';
+					node += '</div>';
+				$('#content').append( node );
+			};
+		});
+	};
+
+	function setupFollowingsPage() {
+		console.log('Setup Followings');
+		$.getJSON('/sn/'+ getUserId() +'/followings0', {}, function (json) {
+
+		});
+	};
+
+
+	function setupGroupPage() {
+		console.log('Setup Group');
+		$.getJSON('/sn/groups0', {}, function (json) {
+
+		});
+	};
 
 
 	function determinePage() {
@@ -221,6 +295,18 @@ jQuery(function( $ ) {
 				console.log('Messages-page detected');
 				setupMessagesPage();
 				break;
+			case 'news':
+				console.log('News-page detected');
+				setupNewsPage();
+				break;
+			case 'followings':
+				console.log('Followings-page detected');
+				setupFollowingsPage();
+				break;
+			case 'group':
+				console.log('Group-page detected');
+				setupGroupPage();
+				break;
 			default:
 				console.log('Custom-page detected');
 		}
@@ -232,22 +318,45 @@ jQuery(function( $ ) {
 
 
 
-	function updateMessages() {
+
+
+// console.log( $(document).scrollTop() + $(document).height() );
+// console.log( $(window).height() );
+
+
+	// setInterval( function(){ 
+
+	// 	console.log( $('#content').scrollTop() );
+
+	//  } , 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+	// function updateMessages() {
 		// $.get('ajax/message.json', function(response){
 		// 	if ( response.new == true ) {
 		// 		console.log('new Message');
 		// 	}
 		// });
-	}
-	function updateFriends() {
+	// }
+	// function updateFriends() {
 		// $.get('ajax/friend.json', function(response){
 		// 	if ( response.new == true ) {
 		// 		console.log('new Friend');
 		// 	} 
 		// });
-	}
-	setInterval( function(){ updateMessages(); } , 5000);
-	setInterval( function(){ updateFriends(); } , 10000);
+	// }
+	// setInterval( function(){ updateMessages(); } , 5000);
+	// setInterval( function(){ updateFriends(); } , 10000);
 
 
 
