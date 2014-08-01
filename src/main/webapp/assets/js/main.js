f7eaf187a19e9c31ee86d0d5591a8baa8e17342e
BishUA <ihor.kozhevnikov@gmail.com>
2014-08-01 12:59:36 +0300

    SN-106 load avatar on homepage

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -1,21 +1,21 @@
-jQuery(function( $ ) {
+jQuery(function ($) {
     'use strict';
 
     $('#formlogin').validate({
-        submitHandler: function(form) {
+        submitHandler: function (form) {
             var login = $('#userlogin').val();
             var pass = $('#userpassword').val();
-            $.post('/sn/index/login',{login:login,pass:pass},function(server_json){
-                if(server_json.status==true) {
+            $.post('/sn/index/login', {login: login, pass: pass}, function (server_json) {
+                if (server_json.status == true) {
                     location.reload();
                 }
                 // else {alert('Wrong login or password')}
-            },'json')
+            }, 'json')
         }
     });
 
     $('#formregister').validate({
-        submitHandler: function(form) {
+        submitHandler: function (form) {
             var name = $('#name').val();
             var surname = $('#surname').val();
             var position = $('#position').val();
@@ -23,21 +23,21 @@ jQuery(function( $ ) {
             var birthday = $('#birthday').val();
             var pass = $('#pass').val();
             var invite = $('#invite').val();
-            $.post('/sn/index/registration',{name:name,surname:surname,position:position,birthday:birthday,email:email,password:pass,invite:invite},function(response){
-                if ( response.status == true ) {
+            $.post('/sn/index/registration', {name: name, surname: surname, position: position, birthday: birthday, email: email, password: pass, invite: invite}, function (response) {
+                if (response.status == true) {
                     location.reload();
                     $('#register-error-msg').hide();
-                } else if ( response.status == false) {
+                } else if (response.status == false) {
                     var msg = 'Server responded with error';
                     $('#register-error-msg').text(msg).fadeIn(500);
-                } else if ( response.status == 'wrongInviteCode' ) {
+                } else if (response.status == 'wrongInviteCode') {
                     var msg = 'Wrong invite code';
                     $('#register-error-msg').text(msg).fadeIn(500);
-                } else if ( response.status == 'wrongLoginPass' ) {
+                } else if (response.status == 'wrongLoginPass') {
                     var msg = 'Wrong login or password';
                     $('#register-error-msg').text(msg).fadeIn(500);
                 }
-            },'json');
+            }, 'json');
         },
         rules: {
             pass: {
@@ -51,25 +51,17 @@ jQuery(function( $ ) {
     });
 
 
-
-
-
-
-
-
-
-    $('#register-btn').click( function() {
+    $('#register-btn').click(function () {
         $('.login-form').hide();
         $('.register-form').fadeIn();
     });
-    $('#back-btn').click( function() {
+    $('#back-btn').click(function () {
         $('.register-form').hide();
         $('.login-form').fadeIn();
     });
 
 
-
-    $('.messages-tab').click(function(event){
+    $('.messages-tab').click(function (event) {
         event.preventDefault();
         var tab = $(this);
         $('.messages-tab').removeClass('active')
@@ -80,33 +72,28 @@ jQuery(function( $ ) {
         $(id).fadeIn();
     });
 
-    $('#exit').click('click', function() {
-        $.get('/sn/user' +getUserId()+ '/exit', {}, function(response){
-            if( response.status == true ) {
+    $('#exit').click('click', function () {
+        $.get('/sn/user' + getUserId() + '/exit', {}, function (response) {
+            if (response.status == true) {
                 location.reload();
             }
-        },'json');
+        }, 'json');
     });
 
 
-
-
-
-
-
-    $('#wall-submit').on('click', function(event){
+    $('#wall-submit').on('click', function (event) {
         event.preventDefault();
         var msg = $('#wall-message').val();
-        if( msg == '') return;
-        $.post('/sn/user' +getUserId()+ '/createPost', {msg:msg}, function(response){
-            if( response.status == true) {
+        if (msg == '') return;
+        $.post('/sn/user' + getUserId() + '/createPost', {msg: msg}, function (response) {
+            if (response.status == true) {
                 location.reload();
             }
         });
     });
 
-    $('#wall-message').on('change keyup', function(){
-        if ( $('#wall-message').val() == '' ) {
+    $('#wall-message').on('change keyup', function () {
+        if ($('#wall-message').val() == '') {
             $('#wall-submit').addClass('inactive');
         } else {
             $('#wall-submit').removeClass('inactive');
@@ -114,23 +101,9 @@ jQuery(function( $ ) {
     });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     function formatDate(msec) {
         var date = new Date(msec);
-        var month = ( '0' + ( date.getMonth()+1 ) ).slice(-2);
+        var month = ( '0' + ( date.getMonth() + 1 ) ).slice(-2);
         var formated = 'at ' + date.getHours() + ':' + date.getMinutes() + ' on ' + date.getDate() + '.' + month + '.' + date.getFullYear();
         return formated;
     };
@@ -138,17 +111,17 @@ jQuery(function( $ ) {
     function getUserId() {
         var url = window.location.href;
         var pos = url.indexOf('user');
-        if ( pos == -1) {
+        if (pos == -1) {
             var id = $.cookie("userId");
             return id;
         }
-        var id = url.slice(pos+4);
+        var id = url.slice(pos + 4);
         return id;
     }
 
     function removePost(id) {
-        $.post('/sn/user' +getUserId()+ '/deletePost', {postId: id}, function(response){
-            if(response.status == true ) {
+        $.post('/sn/user' + getUserId() + '/deletePost', {postId: id}, function (response) {
+            if (response.status == true) {
                 location.reload();
             }
         });
@@ -158,20 +131,12 @@ jQuery(function( $ ) {
     function sendPrivateMessage() {
         var url = window.location.href;
         var pos = url.indexOf('user');
-        var id = url.slice(pos+4);
-        $.post('/sn/pm/sendMessage', {to:1, msg:"thids idsbfb dgdg fgdg df"}, function(json) {
+        var id = url.slice(pos + 4);
+        $.post('/sn/pm/sendMessage', {to: 1, msg: "thids idsbfb dgdg fgdg df"}, function (json) {
 
         });
     };
     // sendPrivateMessage();
-
-
-
-
-
-
-
-
 
 
     function setupHomePage() {
@@ -179,26 +144,31 @@ jQuery(function( $ ) {
         var page = 0;
         var fullname = '';
         var position = '';
+        var ava = new Image();
 
-        $.getJSON('/sn/user'+ getUserId() +'/getUser', {}, function(json) {
-            $('#user-name').text( json.name );
-            $('#user-surname').text( json.surname );
-            $('#user-position').text( json.position );
+        $.getJSON('/sn/user' + getUserId() + '/getUser', {}, function (json) {
+            $('#user-name').text(json.name);
+            $('#user-surname').text(json.surname);
+            $('#user-position').text(json.position);
             fullname = json.name + ' ' + json.surname;
         });
 
-        $.getJSON('/sn/user' +getUserId()+ '/interests', {}, function(json) {
+        var str = '/sn/user' + getUserId() + '/getAvatar';
+        document.getElementById("avatar").src = str;
+
+        $.getJSON('/sn/user' + getUserId() + '/interests', {}, function (json) {
             var str = [];
             for (var i = 0; i < json.interests.length; i++) {
-                str.push( json.interests[i].interest );
-            };
+                str.push(json.interests[i].interest);
+            }
+            ;
             str = str.join(', ');
-            $('#user-hobbies').text( str );
+            $('#user-hobbies').text(str);
         });
 
 
-        function _loadWallPosts () {
-            $.getJSON('/sn/user' + getUserId() + '/posts'+ page , {}, function(json) {
+        function _loadWallPosts() {
+            $.getJSON('/sn/user' + getUserId() + '/posts' + page, {}, function (json) {
                 _makeupWallPosts(json);
             });
             page++;
@@ -208,20 +178,21 @@ jQuery(function( $ ) {
             for (var i = 0; i < json.posts.length; i++) {
                 var node = '<div class="post" id="' + json.posts[i].postId + '">';
                 node += '<div class="post-photo">';
-                node +=	'<a href="#">';
+                node += '<a href="#">';
                 node += '<img src="/assets/img/Mt-8_dgwlHM.jpg" alt="">';
                 node += '</a>';
                 node += '</div>';
                 node += '<div class="post-message">';
                 node += '<p class="post-name"><a href="">' + fullname + '</a></p>';
                 node += '<span class="remove-post"></span>';
-                node +=	'<p>' + json.posts[i].post + '</p>';
-                node += '<span class="post-meta">'+formatDate( json.posts[i].time )+'</span>';
+                node += '<p>' + json.posts[i].post + '</p>';
+                node += '<span class="post-meta">' + formatDate(json.posts[i].time) + '</span>';
                 node += '</div>';
                 node += '</div>';
-                $('#wall').append( node );
-            };
-            $('.remove-post').on('click', function(){
+                $('#wall').append(node);
+            }
+            ;
+            $('.remove-post').on('click', function () {
                 var id = $(this).parents('.post').attr('id');
                 removePost(id);
             });
@@ -229,30 +200,30 @@ jQuery(function( $ ) {
 
         _loadWallPosts();
 
-        setTimeout( function(){ _loadWallPosts(); } , 5000);
+        setTimeout(function () {
+            _loadWallPosts();
+        }, 5000);
 
     }
-
 
 
     function setupMessagesPage() {
         console.log('Setup Messages');
-        $.getJSON('/sn/pm/received0', {}, function(json) {
+        $.getJSON('/sn/pm/received0', {}, function (json) {
             for (var i = 0; i < json.privateMessages.length; i++) {
                 var node = '<span><span>';
 
-                $('#incoming').append( $(node) );
-            };
+                $('#incoming').append($(node));
+            }
+            ;
         });
-        $.getJSON('/sn/pm/sent0', {}, function(json) {
+        $.getJSON('/sn/pm/sent0', {}, function (json) {
             for (var i = 0; i < json.privateMessages.length; i++) {
 
-            };
+            }
+            ;
         });
     }
-
-
-
 
 
     function setupColleaguesPage() {
@@ -262,15 +233,15 @@ jQuery(function( $ ) {
             for (var i = 0; i < json.users.length; i++) {
                 list += '<li class="user-entry">';
                 list += '<div class="textual">';
-                list += '<div class="user-name"><a href="/sn/user'+ json.users[i].id+'"><span></span>' + json.users[i].name + ' '+ json.users[i].surname +'</a></div>';
+                list += '<div class="user-name"><a href="/sn/user' + json.users[i].id + '"><span></span>' + json.users[i].name + ' ' + json.users[i].surname + '</a></div>';
                 list += '<div class="user-position"><span>Position </span>' + json.users[i].position + '</div>';
                 list += '</li>';
-            };
+            }
+            ;
             list += '</ul>';
-            $('#listOfUsers').append( $(list) );
+            $('#listOfUsers').append($(list));
         });
     };
-
 
 
     function setupNewsPage() {
@@ -286,19 +257,20 @@ jQuery(function( $ ) {
                 node += '<div class="news-message">';
                 // node += '<p class="news-name"><a href="">' + fullname + '</a></p>';
                 // node += '<span class="remove-post"></span>';
-                node +=	'<p>' + json.posts[i].post + '</p>';
-                node += '<span class="post-meta">'+formatDate( json.posts[i].time )+'</span>';
+                node += '<p>' + json.posts[i].post + '</p>';
+                node += '<span class="post-meta">' + formatDate(json.posts[i].time) + '</span>';
                 node += '</div>';
                 node += '</div>';
-                $('#content').append( node );
-            };
+                $('#content').append(node);
+            }
+            ;
         });
     };
 
 
     function setupFollowingsPage() {
         console.log('Setup Followings');
-        $.getJSON('/sn/'+ getUserId() +'/followings0', {}, function (json) {
+        $.getJSON('/sn/' + getUserId() + '/followings0', {}, function (json) {
 
         });
     };
@@ -323,7 +295,7 @@ jQuery(function( $ ) {
         var year = $('#useryear');
         var avatar = $('#useravatar');
 
-        $.get('/sn/user'+ getUserId() +'/getUser', {}, function (json) {
+        $.get('/sn/user' + getUserId() + '/getUser', {}, function (json) {
             name.val(json.name);
             surname.val(json.surname);
             position.val(json.position);
@@ -332,10 +304,10 @@ jQuery(function( $ ) {
             month.val(bday[1]);
             date.val(bday[2]);
         });
-        $.getJSON('/sn/user' + getUserId() + '/interests', {}, function(json) {
+        $.getJSON('/sn/user' + getUserId() + '/interests', {}, function (json) {
             var str = [];
             for (var i = 0; i < json.interests.length; i++) {
-                str.push( json.interests[i].interest );
+                str.push(json.interests[i].interest);
             }
             str = str.join(', ');
 
@@ -343,17 +315,16 @@ jQuery(function( $ ) {
         });
 
         $('#usereditform').validate({
-            submitHandler: function(form) {
-                $.post('/sn/user0/edit', { name:name.val(), surname:surname.val(), position:position.val(), interests:interests.val(), date: date.val(), month:month.val(), year:year.val() }, function (json) {
-                    // $.post('/sn/index/login',{}, function (json) {
-                    // if(json.status == true ) {
-                    // 	location.reload();
-                    // }
-                });
+            submitHandler: function (form) {
+                $.post('/sn/user0/edit', { name: name.val(), surname: surname.val(), position: position.val(), interests: interests.val(), date: date.val(), month: month.val(), year: year.val() }, function (json) {
+                     if (json.status == true) {
+                             window.location.href = '/';
+                         }
+                     });
+
             }
         });
     };
-
 
 
 // var name = '<p>'+ json.name + '<p>';
@@ -361,10 +332,9 @@ jQuery(function( $ ) {
 // var img = '<img src="' + json.image + ' ">';
 
 
-
     function determinePage() {
         var pageId = $('body').attr('id');
-        switch(pageId) {
+        switch (pageId) {
             case 'home':
                 console.log('Home-page detected');
                 setupHomePage();
@@ -399,27 +369,6 @@ jQuery(function( $ ) {
     }
 
     determinePage();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     /*	var $wrapper = $(".main-section"),
